fd6f57e20ff9fab70e11457f487ea48bd561f45e
RUMI\Rumi <rizwan.cse33@gmail.com>
2025-05-22 21:58:27 +0200

    NEW: added FunctionParameters class with enum values

---
+++ SimpleGraphCalculator/Models/FunctionParameters.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleGraphCalculatorApp.Models
+{   
+    public enum FunctionType 
+    { 
+        Sin, 
+        Cos, 
+        Sinc
+    }
+
+    public class FunctionParameters
+    {
+        public FunctionType Type { get; set; } = FunctionType.Sin;
+        public double Amplitude { get; set; } = 1.0;
+        public double Frequency { get; set; } = 1.0;
+        public double Phase { get; set; } = 0.0;
+        public double RangeStart { get; set; } = -10.0;
+        public double RangeEnd { get; set; } = 10.0;
+    }
+}
