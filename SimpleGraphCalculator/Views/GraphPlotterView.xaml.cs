aa988503f9af953d92f5ab816f08fc5b763b3cb3
RUMI\Rumi <rizwan.cse33@gmail.com>
2025-05-22 22:14:36 +0200

    NEW: added GraphPlotterView.xaml

---
+++ SimpleGraphCalculator/Views/GraphPlotterView.xaml.cs
@@ -0,0 +1,17 @@
+﻿using SimpleGraphCalculatorApp.ViewModels;
+using System.Windows;
+
+namespace SimpleGraphCalculator.Views
+{
+    /// <summary>
+    /// Interaction logic for GraphPlotterView.xaml
+    /// </summary>
+    public partial class GraphPlotterView : Window
+    {
+        public GraphPlotterView()
+        {
+            InitializeComponent();
+            DataContext = new GraphPlotterViewModel();
+        }
+    }
+}
