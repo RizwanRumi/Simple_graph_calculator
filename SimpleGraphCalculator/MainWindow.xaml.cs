1967877ade41d1ade548cead5d6950f9aa317ceb
RUMI\Rumi <rizwan.cse33@gmail.com>
2025-05-22 22:00:48 +0200

    EDIT: added Sample Graph Calculator GUI Design with DataContext

--- SimpleGraphCalculator/MainWindow.xaml.cs
+++ SimpleGraphCalculator/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using SimpleGraphCalculatorApp.ViewModels;
 
 namespace SimpleGraphCalculatorApp
 {
@@ -23,6 +24,7 @@ namespace SimpleGraphCalculatorApp
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new MainWindowViewModel();
         }
     }
 }
