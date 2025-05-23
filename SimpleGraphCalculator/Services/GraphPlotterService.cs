f7f9adec9ec2e335d62c5e290d686cc385a6f932
RUMI\Rumi <rizwan.cse33@gmail.com>
2025-05-23 22:45:32 +0200

    NEW: added GraphPlotterService

---
+++ SimpleGraphCalculator/Services/GraphPlotterService.cs
@@ -0,0 +1,32 @@
+﻿using SimpleGraphCalculatorApp.Interfaces;
+using SimpleGraphCalculatorApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleGraphCalculatorApp.Services
+{
+    public class GraphPlotterService
+    {
+        private readonly IFunction _currentFunction;
+
+        // Constructor injection   
+        public GraphPlotterService(FunctionFactory factory, double amplitude, double frequency, double phase)
+        {
+            if (factory == null)
+                throw new ArgumentNullException(nameof(factory));
+
+            _currentFunction = factory.CreateFunction(amplitude, frequency, phase);
+        }
+                
+        public double CalculateFunctionValue(double x)
+        {
+            if (_currentFunction == null)
+                throw new InvalidOperationException("Function is not initialized.");
+
+            return _currentFunction.Calculate(x);
+        }
+    }
+}
