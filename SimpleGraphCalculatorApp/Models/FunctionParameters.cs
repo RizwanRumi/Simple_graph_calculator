aa49db159818d06151e31bf7cecdd4d6dbe35c44
RUMI\Rumi <rizwan.cse33@gmail.com>
2025-05-25 13:10:49 +0200

    EDIT: added textbox input validation only for double value

--- SimpleGraphCalculatorApp/Models/FunctionParameters.cs
+++ SimpleGraphCalculatorApp/Models/FunctionParameters.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SimpleGraphCalculatorApp.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,12 +14,62 @@ namespace SimpleGraphCalculatorApp.Models
         Sinc
     }
 
-    public class FunctionParameters
+    public class FunctionParameters : ViewModelBase
     {        
-        public double Amplitude { get; set; } = 1.0;
-        public double Frequency { get; set; } = 1.0;
-        public double Phase { get; set; } = 0.0;
-        public double RangeStart { get; set; } = -10.0;
-        public double RangeEnd { get; set; } = 10.0;
+        private double _amplitude = 1.0;
+        private double _frequency = 1.0;
+        private double _phase = 1.0;
+        private double _rangeStart = -10.0;
+        private double _rangeEnd = 10.0;
+  
+        public double Amplitude
+        {
+            get => _amplitude;
+            set
+            {
+                _amplitude = value;
+                OnPropertyChanged(nameof(Amplitude));
+            }
+        }
+        
+        public double Frequency
+        {
+            get => _frequency;
+            set
+            {
+                _frequency = value;
+                OnPropertyChanged(nameof(Frequency));
+            }
+        }
+
+        public double Phase
+        {
+            get => _phase;
+            set
+            {
+                _phase = value;
+                OnPropertyChanged(nameof(Phase));
+            }
+        }
+        
+        public double RangeStart
+        {
+            get => _rangeStart;
+            set
+            {
+                _rangeStart = value;
+                OnPropertyChanged(nameof(RangeStart));
+            }
+        }
+
+        public double RangeEnd
+        {
+            get => _rangeEnd;
+            set
+            {
+                _rangeEnd = value;
+                OnPropertyChanged(nameof(RangeEnd));
+            }
+        }
     }
 }
--- SimpleGraphCalculatorApp/Views/GraphPlotterView.xaml.cs
+++ SimpleGraphCalculatorApp/Views/GraphPlotterView.xaml.cs
@@ -3,6 +3,9 @@ using SimpleGraphCalculatorApp.ViewModels;
 using SimpleGraphCalculatorApp.Interfaces;
 using SimpleGraphCalculatorApp.Models;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Linq;
 
 namespace SimpleGraphCalculatorApp.Views
 {
@@ -20,6 +23,52 @@ namespace SimpleGraphCalculatorApp.Views
             FunctionParameters parameters = SettingsService.Load(); // Load on startup;
 
             DataContext = new GraphPlotterViewModel(functionFactory, messageService, parameters);
+        }
+
+        private void DoubleOnly_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            // Allow only digits, decimal point, and minus sign
+            char c = e.Text[0];
+
+            if (!char.IsDigit(c) && c != '.' && c != '-')
+            {
+                e.Handled = true;
+                return;
+            }
+
+            TextBox textBox = sender as TextBox;
+
+            // Don't allow multiple decimal points
+            if (c == '.' && textBox.Text.Contains('.'))
+            {
+                e.Handled = true;
+                return;
+            }
+
+            // Don't allow minus sign except at the beginning
+            if (c == '-' && textBox.SelectionStart != 0)
+            {
+                e.Handled = true;
+                return;
+            }
+
+            // Don't allow multiple minus signs
+            if (c == '-' && textBox.Text.Contains('-'))
+            {
+                e.Handled = true;
+                return;
+            }
+        }
+
+        // Add this new event handler to handle empty TextBox
+        private void TextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+                        
+            if (string.IsNullOrWhiteSpace(textBox.Text))
+            {
+                textBox.Text = "1.0";
+            }
         }
     }
 }
