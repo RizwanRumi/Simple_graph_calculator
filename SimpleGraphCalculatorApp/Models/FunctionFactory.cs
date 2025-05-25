11e414ae8f7c6b0cbcddc05953cc5791fe136aee
RUMI\Rumi <rizwan.cse33@gmail.com>
2025-05-25 21:12:28 +0200

    EDIT: deleted extra libraries from FunctionFactory class

--- SimpleGraphCalculatorApp/Models/FunctionFactory.cs
+++ SimpleGraphCalculatorApp/Models/FunctionFactory.cs
@@ -1,12 +1,4 @@
-﻿using SimpleGraphCalculatorApp.Interfaces;
-using SimpleGraphCalculatorApp.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SimpleGraphCalculatorApp.Models
+﻿namespace SimpleGraphCalculatorApp.Models
 {
     public abstract class FunctionFactory
     {
